68905c6472c72330dcb2ed0450efec0d9564756f
Veronica <VMGranite@gmail.com>
2024-05-15 22:30:00 +0200

    utilized @login_required

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,21 +1,22 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import modelform_factory
 
 from meetings.models import Meeting, Room
 
-
+@login_required
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
-
+@login_required
 def rooms_list(request):
     return render(request, "meetings/rooms.html", {"rooms": Room.objects.all()})
 
 
 MeetingForm = modelform_factory(Meeting, exclude=[])
 
-
+@login_required
 def new(request):
     if request.method == "POST":
         form = MeetingForm(request.POST)
@@ -26,7 +27,7 @@ def new(request):
         form = MeetingForm()
     return render(request, "meetings/new.html", {"form": form})
 
-
+@login_required
 def edit(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     if request.method == "POST":
@@ -38,7 +39,7 @@ def edit(request, id):
         form = MeetingForm(instance=meeting)
     return render(request, "meetings/edit.html", {"form": form})
 
-
+@login_required
 def delete(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     if request.method == "POST":
