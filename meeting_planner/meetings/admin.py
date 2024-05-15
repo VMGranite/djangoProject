3b570dfde2e92b8cfcf6d336738cab34ae5fab95
Veronica <VMGranite@gmail.com>
2024-05-15 22:42:49 +0200

    customize Admin interface

--- meeting_planner/meetings/admin.py
+++ meeting_planner/meetings/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from meetings.models import Meeting, Room
-# Register your models here.
 
-admin.site.register(Meeting)
 admin.site.register(Room)
+
+@admin.register(Meeting)
+class MeetingAdmin(ModelAdmin):
+    model = Meeting
+    list_display = ('id', 'title', 'date', 'start_time', 'room')
+    ordering = ('date', 'start_time')
+    search_fields = ('title',)
