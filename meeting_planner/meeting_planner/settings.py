f92c862305549fd851ce2c57ba63a3c636ab4bef
Veronica <VMGranite@gmail.com>
2024-05-15 22:37:47 +0200

    Commented Logging Ability

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -127,3 +127,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = '/auth/login/'
+
+# Not for Production code
+# LOGGING = {
+#     'version': 1,
+#     'handlers': {
+#         'console': {
+#             'class': 'logging.StreamHandler',
+#         },
+#     },
+#     'loggers': {
+#         'django.db.backends': {
+#             'level': 'DEBUG',
+#         },
+#     },
+#     'root': {
+#         'handlers': ['console'],
+#     }
+# }
